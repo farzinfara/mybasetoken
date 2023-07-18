0faed10478a6a86581bfaa9f2b6ced423a8d5baa
farzin <94672107+farzinfara@users.noreply.github.com>
2023-07-19 03:41:16 +0400

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x637fbb5f573374c32d49e6037d315e72c59f59a4";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
